12e5509867654064c68620c8339eb82fcc2e3058
杨健 <470253737@qq.com>
2017-03-29 11:40:02 +0800

    1
    1

--- js/qiye.js
+++ js/qiye.js
@@ -42,6 +42,138 @@ jQuery.extend({
 $(function(){
    $(".liebiao").on("click",function(){
    	 $(".liebio-small").slideToggle();
+   });
+
+//客服qq聊天
+    function browser() {
+      var u = navigator.userAgent.toLowerCase();
+      var app = navigator.appVersion.toLowerCase();
+      return {
+          txt: u, // 浏览器版本信息
+          version: (u.match(/.+(?:rv|it|ra|ie)[\/: ]([\d.]+)/) || [])[1], // 版本号       
+          msie: /msie/.test(u) && !/opera/.test(u), // IE内核
+          mozilla: /mozilla/.test(u) && !/(compatible|webkit)/.test(u), // 火狐浏览器
+          safari: /safari/.test(u) && !/chrome/.test(u), //是否为safair
+          chrome: /chrome/.test(u), //是否为chrome
+          opera: /opera/.test(u), //是否为oprea
+          presto: u.indexOf('presto/') > -1, //opera内核
+          webKit: u.indexOf('applewebkit/') > -1, //苹果、谷歌内核
+          gecko: u.indexOf('gecko/') > -1 && u.indexOf('khtml') == -1, //火狐内核
+          mobile: !!u.match(/applewebkit.*mobile.*/), //是否为移动终端
+          ios: !!u.match(/\(i[^;]+;( u;)? cpu.+mac os x/), //ios终端
+          android: u.indexOf('android') > -1, //android终端
+          iPhone: u.indexOf('iphone') > -1, //是否为iPhone
+          iPad: u.indexOf('ipad') > -1, //是否iPad
+          webApp: !!u.match(/applewebkit.*mobile.*/) && u.indexOf('safari/') == -1 //是否web应该程序，没有头部与底部
+      };
+  }
+  var timeout;
+  function open_appstore() {
+    var b=browser();
+    if(b.ios||b.iPhone||b.iPad){
+      window.location="itms-apps://itunes.apple.com/cn/app/qq-2011/id444934666?mt=8";
+    }else if(b.android){
+      window.location="http://wpa.qq.com/msgrd?v=3&uin=3001662672&site=qq&menu=yes";
+    }else{
+      window.location="http://wpa.qq.com/msgrd?v=3&uin=3001662672&site=qq&menu=yes";
+    }
+  } 
+   
+   $(".yg").on("click",function(){
+   	    var b=browser();
+	    if(b.ios||b.iPhone||b.iPad){
+	      window.location="mqq://im/chat?chat_type=wpa&uin=3001662672&version=1&src_type=web";
+	    }else if(b.android){
+	     window.location="http://wpa.qq.com/msgrd?v=3&uin=3001662672&site=qq&menu=yes";
+	    }else{
+	      window.location="http://wpa.qq.com/msgrd?v=3&uin=3001662672&site=qq&menu=yes";
+	    }
+	    timeout = setTimeout('open_appstore()', 300);
    })
+
+   $(".cb").on("click",function(){
+   	    var b=browser();
+	    if(b.ios||b.iPhone||b.iPad){
+	      window.location="mqq://im/chat?chat_type=wpa&uin=3001678739&version=1&src_type=web";
+	    }else if(b.android){
+	     window.location="http://wpa.qq.com/msgrd?v=3&uin=3001678739&site=qq&menu=yes";
+	    }else{
+	      window.location="http://wpa.qq.com/msgrd?v=3&uin=3001678739&site=qq&menu=yes";
+	    }
+	    timeout = setTimeout('open_appstore()', 300);
+   })
+   
+   $(".htn").on("click",function(){
+   	    var b=browser();
+	    if(b.ios||b.iPhone||b.iPad){
+	      window.location="mqq://im/chat?chat_type=wpa&uin=3001671837&version=1&src_type=web";
+	    }else if(b.android){
+	     window.location="http://wpa.qq.com/msgrd?v=3&uin=3001671837&site=qq&menu=yes";
+	    }else{
+	      window.location="http://wpa.qq.com/msgrd?v=3&uin=3001671837&site=qq&menu=yes";
+	    }
+	    timeout = setTimeout('open_appstore()', 300);
+   })
+
+   $(".zsf").on("click",function(){
+   	    var b=browser();
+	    if(b.ios||b.iPhone||b.iPad){
+	      window.location="mqq://im/chat?chat_type=wpa&uin=3001670816&version=1&src_type=web";
+	    }else if(b.android){
+	     window.location="http://wpa.qq.com/msgrd?v=3&uin=3001670816&site=qq&menu=yes";
+	    }else{
+	      window.location="http://wpa.qq.com/msgrd?v=3&uin=3001670816&site=qq&menu=yes";
+	    }
+	    timeout = setTimeout('open_appstore()', 300);
+   })
+
+   $(".zzr").on("click",function(){
+   	    var b=browser();
+	    if(b.ios||b.iPhone||b.iPad){
+	      window.location="mqq://im/chat?chat_type=wpa&uin=3001664228&version=1&src_type=web";
+	    }else if(b.android){
+	     window.location="http://wpa.qq.com/msgrd?v=3&uin=3001664228&site=qq&menu=yes";
+	    }else{
+	      window.location="http://wpa.qq.com/msgrd?v=3&uin=3001664228&site=qq&menu=yes";
+	    }
+	    timeout = setTimeout('open_appstore()', 300);
+   })
+
+   $(".gym").on("click",function(){
+   	    var b=browser();
+	    if(b.ios||b.iPhone||b.iPad){
+	      window.location="mqq://im/chat?chat_type=wpa&uin=3001655956&version=1&src_type=web";
+	    }else if(b.android){
+	     window.location="http://wpa.qq.com/msgrd?v=3&uin=3001655956&site=qq&menu=yes";
+	    }else{
+	      window.location="http://wpa.qq.com/msgrd?v=3&uin=3001655956&site=qq&menu=yes";
+	    }
+	    timeout = setTimeout('open_appstore()', 300);
+   })
+
+   $(".sfy").on("click",function(){
+   	    var b=browser();
+	    if(b.ios||b.iPhone||b.iPad){
+	      window.location="mqq://im/chat?chat_type=wpa&uin=3001639860&version=1&src_type=web";
+	    }else if(b.android){
+	     window.location="http://wpa.qq.com/msgrd?v=3&uin=3001639860&site=qq&menu=yes";
+	    }else{
+	      window.location="http://wpa.qq.com/msgrd?v=3&uin=3001639860&site=qq&menu=yes";
+	    }
+	    timeout = setTimeout('open_appstore()', 300);
+   })
+
+   $(".nxr").on("click",function(){
+   	    var b=browser();
+	    if(b.ios||b.iPhone||b.iPad){
+	      window.location="mqq://im/chat?chat_type=wpa&uin=3001658062&version=1&src_type=web";
+	    }else if(b.android){
+	     window.location="http://wpa.qq.com/msgrd?v=3&uin=3001658062&site=qq&menu=yes";
+	    }else{
+	      window.location="http://wpa.qq.com/msgrd?v=3&uin=3001658062&site=qq&menu=yes";
+	    }
+	    timeout = setTimeout('open_appstore()', 300);
+   })
+
 })
 

1394fbe229687be28a6b4c11745619c6304f2af6
杨健 <470253737@qq.com>
2017-04-01 12:49:17 +0800

    1
    1

--- guidance1/js/guidance.js
+++ guidance1/js/guidance.js
@@ -1,4 +1,24 @@
 $(function(){
-    $(".title-box").height();
-    console.log($(".title-box").height())
+    
+    window.onscroll=function(){
+    	var obj=document.body.scrollTop?document.body:document.documentElement//真则用doncument.                                                  body, 假则用document.documentElement
+        var scrollTop=obj.scrollTop;
+
+        if(scrollTop>650){
+           $(".anniu").slideDown();
+        }
+        if(scrollTop<650){
+           $(".anniu").slideUp();
+        };
+    }
+     $(".anniu").on("click",function(){
+        	$("body").animate({
+        		scrollTop:"0"
+        	});
+        	$("html").animate({
+        		scrollTop:"0"
+        	});
+           
+            
+    })
 })

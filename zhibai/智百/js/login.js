53a85d62cefacc67dcb3ed9ab5ba7c99b45b7be4
杨健 <470253737@qq.com>
2017-05-23 09:04:15 +0800

    111
    11

---
+++ zhibai/智百/js/login.js
@@ -0,0 +1,28 @@
+﻿$(function(){
+
+   var nub=0;
+   $(".userName").focus(function () {
+        if ($(this).val() == "请输入账号") {
+            $(this).val("");
+            $(this).css({
+                "color":"#333333"
+            });
+        } else {
+            return;
+        }   
+    });
+
+   $(".userName").on("blur", function () {
+        if (!$(this).val()) {    
+            $(this).css({
+                "color": "#C9C9C9"
+            });
+            $(this).val("请输入账号");
+             $(this).attr("str", "")
+            nub = 0;
+        } else {     
+             $(this).attr("str", $(this).val())
+            nub = 1;
+        }
+    });
+})

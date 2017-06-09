6bfced70f309f1462a1fa5d4c7c732d391ed5dbe
杨健 <470253737@qq.com>
2017-06-09 19:01:07 +0800

    11
    11

--- zhibai/js/login.js
+++ zhibai/js/login.js
@@ -1,28 +1,96 @@
-﻿$(function(){
+$(function(){
 
-   var nub=0;
-   $(".userName").focus(function () {
-        if ($(this).val() == "请输入账号") {
-            $(this).val("");
-            $(this).css({
-                "color":"#333333"
-            });
-        } else {
-            return;
-        }   
+// 正则和提示账号
+	$("input[name='UserName']").keyup(function(){
+		$(this).val($(this).val().replace(/[^\w]/g,''));
+	});
+  
+// 正则和提示密码
+	$("input[name='UserPass']").keyup(function(){
+		$(this).val($(this).val().replace(/[^\w]/g,''));
+	});
+	//获取localstorage值
+	var userData=getData();
+	$("input[name='UserName']").val(userData.UserName);
+	$("input[name='UserPass']").val(userData.UserPass);
+	if(userData){
+		$("input[type='checkbox']").prop("checked","checked");
+	}else{
+		$("input[type='checkbox']").removeProp("checked");
+	}
+	
+	
+    var nub=0;
+    var nub1=0;
+    var token;
+    var flag = true;
+    $("input[name='UserName']").blur(function(){
+    	if($(this).val()==""){
+    		nub=0;
+    	}else{
+    		nub=1;
+    	}
     });
-
-   $(".userName").on("blur", function () {
-        if (!$(this).val()) {    
-            $(this).css({
-                "color": "#C9C9C9"
-            });
-            $(this).val("请输入账号");
-             $(this).attr("str", "")
-            nub = 0;
-        } else {     
-             $(this).attr("str", $(this).val())
-            nub = 1;
-        }
+    $("input[name='UserPass']").blur(function(){
+    	if($(this).val()==""){
+    		nub1=0;
+    	}else{
+    		nub1=1;
+    	}
     });
+    $("#login").click(function(){
+    	if(flag){
+    		flag=false;
+    		var obj = {};
+            $("input[name='UserName']").blur();
+            $("input[name='UserPass']").blur();
+            obj[$("input[name='UserName']").attr('name')] = $("input[name='UserName']").val();
+            obj[$("input[name='UserPass']").attr('name')] = $("input[name='UserPass']").val();
+            obj["ValiCode"] = "zzzzzz";
+            if (nub == 0 || nub1 == 0) {
+                $(".tip").html("请正确填写以上信息!");
+                $(".tip").fadeIn();
+                setTimeout("codefans()",1000);
+                flag = true;
+            } else {
+                $(".popup-inner-loading").show();
+                $(".popup").show();           
+                $(".tip").html("");
+                $.post("http://localhost:25000/api/account/Login", obj,
+                    function (data) {
+                        if (data.issuccess == 1) {
+                        	if($(".checkBox").is(':checked')){
+                        		localStorage.zhibaiUser=JSON.stringify(obj);
+                        	}else{
+                        		localStorage.zhibaiUser=JSON.stringify("");
+                        	}
+                            token = data.message;
+                            document.location = "projectCenter.html";
+                        } else {
+                            flag = true;
+                            $(".popup-inner-loading").hide();
+                            $(".popup").hide();
+                            $(".tip").fadeIn();
+                            $(".tip").html(data.message);
+                            setTimeout("codefans()",1000);
+                        }
+                    }
+                );
+            }
+    	}else{
+    		return;
+    	}
+    }); 
 })
+
+function codefans(){
+    $(".tip").fadeOut()
+}
+ function getData() {
+    var arr=localStorage.zhibaiUser?JSON.parse(localStorage.zhibaiUser):[];
+    return arr;
+}
+
+
+
+
